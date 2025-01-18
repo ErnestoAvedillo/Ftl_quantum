57ff2e245c81a5eb0332f06ddcc1e9c4638dd7c0
ernesto.avedillo <eavedillo@yahoo.es>
2025-01-18 16:24:59 +0100

    remove my token from src

--- ex1/main.py
+++ ex1/main.py
@@ -1,6 +1,6 @@
 from qiskit_ibm_runtime import QiskitRuntimeService
+from ..setings import MY_API_TOKEN
 
-MY_API_TOKEN = "dc09a3509c7c08a114cb746820482602d4d734ffd823c7452474d91374bddd84eeb579213a440882e0fc14e7a1ba2c0f45e663a7e52145c5b01862de30bcd6af"
 QiskitRuntimeService.save_account(
     channel='ibm_quantum',
     token=MY_API_TOKEN,  # Replace with your actual token
